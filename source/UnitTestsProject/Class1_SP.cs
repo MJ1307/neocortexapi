1e71b1b1a05dd67d410aca242acc52caa64c5fce
Muhammad Jasim <muhammad.suhail@stud.fra-uas.de>
2022-02-03 15:08:09 +0100

    initiate

---
+++ source/UnitTestsProject/Class1_SP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTM_SP_tm
+{
+    internal class Class1_SP
+    {
+        public void Deserialize()
+        {
+
+        }
+        public void Serealize()
+        {
+
+        }
+    }
+
+}
